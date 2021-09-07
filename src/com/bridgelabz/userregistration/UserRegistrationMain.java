7f67c6b69b245e0c85c8fca10dd6877bb6837f14
S-P-RAJAT <rajathsiddeshwar@gmail.com>
2021-09-08 01:12:33 +0530

    MODIFIED: To check at least one uppercase character exists in password

--- src/com/bridgelabz/userregistration/UserRegistrationMain.java
+++ src/com/bridgelabz/userregistration/UserRegistrationMain.java
@@ -8,10 +8,10 @@ public static void main(String[] args) {
 	UserRegistration newUser = new UserRegistration();
 	System.out.println("User Registration:");
 
-	newUser.setFirstName();
-	newUser.setLastName();
-	newUser.setEmail();
-	newUser.setMobileNumber();
+//	newUser.setFirstName();
+//	newUser.setLastName();
+//	newUser.setEmail();
+//	newUser.setMobileNumber();
 	newUser.setPassword();
 	UserRegistration.scanner.close();
 }
